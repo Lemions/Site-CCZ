a406fe89b4525b9523b4ed17be5b924d69fc3e19
Leonardo Lemos <leonardolemos136@gmail.com>
2020-10-26 14:44:45 -0200

    Commit inicial

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SiteCCZ.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace SiteCCZ.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IConfiguration _config;
+
+        public AdminController(ILogger<AdminController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            ViewData["Nome"] = claims.ToArray()[1].Value;
+
+            return View();
+        }
+
+        public IActionResult Painel_adm()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            ViewData["Nome"] = claims.ToArray()[1].Value;
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if ((model.Email == "thate7061@gmail.com") && (model.Senha == "123456"))
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, model.Email),
+                    new Claim("FullName", "Thais"),
+                    new Claim(ClaimTypes.Role, "Administrador"),
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties{};
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                return RedirectToAction("Painel_adm");
+            }
+            else
+            {
+                ModelState.AddModelError("", "E-mail e/ou Senha Inválidos!!");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteCCZ.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "E-mail de Acesso", Prompt = "{0}")]
+        [Required(ErrorMessage = "Por favor, informe seu E-mail de Acesso")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um E-mail Válido")]
+        [StringLength(100, ErrorMessage = "Seu E-mail de acesso não possui mais de 100 caracteres")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha", Prompt = "{0}")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Por favor, informe sua Senha")]
+        [StringLength(10, ErrorMessage = "Sua Senha não possui mais de {1} caracteres")]
+        public string Senha { get; set; }
+    }
+}
